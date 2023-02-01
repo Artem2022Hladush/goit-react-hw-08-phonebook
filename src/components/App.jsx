0fd433b62dccb5a5ddc811e4666d152c35e28b14
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-01 17:45:02 +0200

    add PrivetRoute, RestrictedRoute, refact: App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,39 @@
-import ContactList from "./ContactList/ContactList";
-import ContactEditor from "./ContactEditor/ContactEditor";
-import Filter from "./Filter/Filter";
+import { useEffect, lazy } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+
+import { refreshUser } from "redux/auth/operations";
+import { selectIsRefreshing } from "redux/auth/selectors";
+
+import { RestrictedRoute } from "./RestrictedRoute";
+import { PrivateRoute } from "./PrivateRoute";
+import { SharedLayout } from "./SharedLayout/SharedLayout";
+
+const Home = lazy(() => import('../pages/Home'));
+const ContactPage = lazy(() => import('../pages/ContactPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
 
 
 
 const App= () => {
-  // const [contacts, setContacts] = useState(() => {
-  //   const savedContacts = window.localStorage.getItem('contacts');
-  //   if(savedContacts !== null){
-  //    const  parsedContacts = JSON.parse(savedContacts);
-  //    return parsedContacts; 
-  //   }
-  //   return [];
-  // });
+const dispatch = useDispatch();
+const isRefreshing = useSelector(selectIsRefreshing)
 
-  // useEffect(()=> {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts))
-  // }, [contacts])
-
+useEffect = (() => {
+  dispatch(refreshUser())
+}, [dispatch])
   return (
-    <div className="container">
-      <h1 className="title">Phonebook</h1>
-    <ContactEditor />
-    <Filter />
-    <h2 className="title">Contacts</h2>
-    <ContactList />
-    </div>
+    <>
+    
+    </>
+    // <div className="container">
+    //   <h1 className="title">Phonebook</h1>
+    // <ContactEditor />
+    // <Filter />
+    // <h2 className="title">Contacts</h2>
+    // <ContactList />
+    // </div>
   )
 };
 
