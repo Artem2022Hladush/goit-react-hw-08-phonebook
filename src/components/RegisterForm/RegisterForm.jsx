539130cf6363df61629845b89c103b917e523042
Артем Гладуш <m1hommage5008@gmail.com>
2023-02-05 15:11:57 +0200

    refact

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { register } from "redux/auth/operations";
 import { TextField, Button, Box } from "@mui/material";
 
@@ -6,24 +7,40 @@ import { TextField, Button, Box } from "@mui/material";
  export const RegisterForm = () => {
 	const dispatch = useDispatch();
 
-	const handleSubmit = e => {
-		e.preventDefault()
-		const form = e.currentTarget;
-		dispatch(
-			register({
-				name: form.elements.name.value,
-				email: form.elements.email.value,
-				password: form.elements.password.value,
-			})
-			);
-			form.reset();
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+
+const handleChange = ({target: {name, value}}) => {
+	switch (name) {
+		case 'name':
+			return setName(value)
+		case 'email':
+			return setEmail(value)
+		case 'password':
+			return setPassword(value)	
+	
+		default:
+			return;
 	}
+}
+
+const handleSubmit = e => {
+	e.preventDefault();
+	dispatch(register({ name: name, email: email, password: password }));
+	setName('');
+	setEmail('');
+	setPassword('');
+ };
+
 	return(
 		<div>
 			<form autoComplete="off" onSubmit={handleSubmit}>
 				<TextField
 				type="text"
 				name="name"
+				value={name}
+				onChange={handleChange}
 				id="standard-basic"
 				label="Name"
 				variant="standard"
@@ -31,6 +48,8 @@ import { TextField, Button, Box } from "@mui/material";
 					<TextField
 				type="email"
 				name="email"
+				value={email}
+				onChange={handleChange}
 				id="standard-basic"
 				label="Email"
 				variant="standard"
@@ -38,6 +57,8 @@ import { TextField, Button, Box } from "@mui/material";
 					<TextField
 				type="password"
 				name="password"
+				value={password}
+				onChange={handleChange}
 				id="standard-basic"
 				label="password"
 				variant="standard"
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { nanoid } from "nanoid";
 
-axios.defaults.baseURL = "https://63c4138ca908563575310e51.mockapi.io"
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = createAsyncThunk(
 	"contacts/fetchAll",
